ce27e37d96e1400dfa0a55184e68466dd5c724eb
Morris235 <dltkdah777@gmail.com>
2025-07-24 14:42:52 +0900

    first commit

---
+++ src/test/java/com/morris/algorithm/AlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morris.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
